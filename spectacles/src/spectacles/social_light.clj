4b2b0909bf25e3b57fb36add921a72814ab81c46
Aaron <tripnaces@gmail.com>
2015-12-05 11:29:18 -0500

    Refinement of colors, timing and sizing

--- spectacles/src/spectacles/social_light.clj
+++ spectacles/src/spectacles/social_light.clj
@@ -5,36 +5,39 @@
 
 (defn setup []
   (q/color-mode :hsb)
+  (q/background 0)
+  (q/frame-rate 2)
+
   (let [wc (WordCram. (quil.applet/current-applet))]
     (-> wc
         ;;(.fromWebPage "https://twitter.com/WinCityMain")
         ;;(.fromTextString (into-array String ["Life In Lights Winchester"]))
         (.fromTextFile "resources/posts.txt")
-        (.withColors (int-array [(q/color 30) (q/color 110) (q/color (q/random 255) 240 200)]))
-        (.sizedByWeight 5 120)
+        (.withColors (int-array [(q/color 220) (q/color 140) (q/color (q/random 255) 240 200)]))
+        (.sizedByWeight 5 80)
         (.withFont"Copse")
-        (.angledBetween (float -1.6) (float 1.6))
-        (.drawAll))))
+        (.angledBetween (float -1.6) (float 1.6)))
+    {:wordcram wc})
+  )
 
-(defn update-state [state] {})
+(defn update-state [state] state)
 
-(defn draw-state [state] {})
+(defn draw-state [state]
+  (if (.hasMore (:wordcram state))
+    (.drawNext (:wordcram state)))
+  state)
 
 (q/defsketch spectacles
   :title "Social Light"
-  :size [1000 600]
+  :size [800 600]
   ; setup function called only once, during sketch initialization.
   :setup setup
   ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
   :middleware [m/fun-mode]
-  ;;  :features [:keep-on-top :present :no-start]
-;;  :features [:no-start]
+  :features [:keep-on-top :present :no-start]
   :bgcolor 0)
 
 
