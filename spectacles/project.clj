ba50f9863809106ce6574e8c2b55b39dc558f716
Aaron <tripnaces@gmail.com>
2015-12-04 21:10:17 -0500

    Correct license as Apache. Add project resource paths for WordCram

--- spectacles/project.clj
+++ spectacles/project.clj
@@ -1,8 +1,15 @@
 (defproject spectacles "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.6"]}
+                 [quil "2.2.6"]]
+
+  :resource-paths [
+                   ;; Wordmaps / WordCram
+                   "resources/wordcram/library/WordCram.jar"
+                   "resources/wordcram/library/cue.language.jar"
+                   "resources/wordcram/library/jsoup-1.7.2.jar"
+                   ]
   )
