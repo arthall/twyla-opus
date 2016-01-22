f09d252927c4c13e4711e190094635fa19d6d0d8
Aaron <tripnaces@gmail.com>
2016-01-22 17:27:11 -0500

    Add base scheduling

--- spectacles/src/spectacles/presenter.clj
+++ spectacles/src/spectacles/presenter.clj
@@ -1,18 +1,18 @@
 (ns spectacles.presenter
   (:require [quil.core :as q]
             [quil.middleware :as m]
-            [spectacles.present-logo]))
+            [spectacles.scheduler]))
 
 (q/defsketch spectacles
   :title "Presentation Cycle"
   :size [1280 720]
-                                        ; setup function called only once, during sketch initialization.
-  :setup spectacles.present-logo/setup
-                                        ; update-state is called on each iteration before draw-state.
-  :update spectacles.present-logo/update-state
-  :draw spectacles.present-logo/draw-state
+
+  :setup spectacles.scheduler/setup
+  :update spectacles.scheduler/update
+  :draw spectacles.scheduler/draw
+
   :middleware [m/fun-mode]
-  :features [:keep-on-top :present]
+;  :features [:keep-on-top :present]
   :bgcolor "#000000")
 
 (defn -main
---
+++ spectacles/src/spectacles/scheduler.clj
@@ -0,0 +1,28 @@
+(ns spectacles.scheduler
+  (:require [spectacles.present-logo]
+            [spectacles.practice]))
+
+(def sketches [{:setup  spectacles.present-logo/setup
+                :update spectacles.present-logo/update
+                :draw   spectacles.present-logo/draw}
+
+               {:setup  spectacles.practice/setup
+                :update spectacles.practice/update
+                :draw   spectacles.practice/draw}])
+
+(def active-sketch (atom {:sketch (first sketches)
+                          :expiration :never}))
+
+(defn setup []
+  ;; determine which is the first sketch to be active and invoke its setup function
+  ((:setup (:sketch @active-sketch))))
+
+(defn update [state]
+  ;; determine whether it's time to swap sketches
+  ;; if not, invoke the active sketch's draw function
+  ;; if so, swap to the new sketch's draw function, initializing with its setup function
+  ((:update (:sketch @active-sketch)) state))
+
+(defn draw [state]
+  ;; invoke the active sketch's draw function
+  ((:draw (:sketch @active-sketch)) state))
