ed7a669e3f51c97b44711f654039d65e739dd909
Art Hall <arthall@gmail.com>
2016-01-26 17:21:00 -0500

    Triangles sketch

---
+++ spectacles/src/spectacles/triangles.clj
@@ -0,0 +1,34 @@
+(ns quil-projects.practice_2
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  (q/smooth)
+  (q/frame-rate 4)
+  {:vdots '()
+   :hdots '()})
+
+(defn draw [state]
+  ;; (q/background 255)
+  (q/no-stroke)
+  (q/fill 255 255 255 10)
+  (q/rect 0 0 (q/width) (q/height))
+  (q/fill (mod (* 20 (q/frame-count)) 200)
+          (mod (* 30 (q/frame-count)) 150)
+          (mod (* 10 (q/frame-count)) 255) 30)
+  (q/translate (/ (q/width) 2) (/ (q/height) 2))
+  (q/rotate (* (q/frame-count) (/ Math/PI 5)))
+  (let [distance (/ (q/height) 3)]
+    (q/triangle 0 0 (- (/ distance 2)) distance (/ distance 2) distance)))
+
+(defn update [state])
+
+(q/defsketch practice
+  :title "Triangles"
+  :size [640 480]
+  :setup setup
+  :draw draw
+  :update update
+  :features [:keep-on-top]
+  :middleware [m/fun-mode])
+
