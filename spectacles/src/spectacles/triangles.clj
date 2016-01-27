058cc4a0a874949b8ca37657e957ceb9063d2dfe
Art Hall <arthall@gmail.com>
2016-01-27 10:58:46 -0500

    Fix namespace and state issues in triangles sketch

--- spectacles/src/spectacles/triangles.clj
+++ spectacles/src/spectacles/triangles.clj
@@ -1,12 +1,11 @@
-(ns quil-projects.practice_2
+(ns spectacles.triangles
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
 (defn setup []
   (q/smooth)
   (q/frame-rate 4)
-  {:vdots '()
-   :hdots '()})
+  {})
 
 (defn draw [state]
   ;; (q/background 255)
@@ -15,20 +14,23 @@
   (q/rect 0 0 (q/width) (q/height))
   (q/fill (mod (* 20 (q/frame-count)) 200)
           (mod (* 30 (q/frame-count)) 150)
-          (mod (* 10 (q/frame-count)) 255) 30)
+          (mod (* 10 (q/frame-count)) 255)
+          30)
   (q/translate (/ (q/width) 2) (/ (q/height) 2))
   (q/rotate (* (q/frame-count) (/ Math/PI 5)))
   (let [distance (/ (q/height) 3)]
-    (q/triangle 0 0 (- (/ distance 2)) distance (/ distance 2) distance)))
+    (q/triangle 0 0 (- (/ distance 2)) distance (/ distance 2) distance))
+  state)
 
-(defn update [state])
+(defn update-state [state]
+  state)
 
 (q/defsketch practice
   :title "Triangles"
   :size [640 480]
   :setup setup
   :draw draw
-  :update update
+  :update update-state
   :features [:keep-on-top]
-  :middleware [m/fun-mode])
+  :middleware [m/fun-mode m/navigation-2d])
 
